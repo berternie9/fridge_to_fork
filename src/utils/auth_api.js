f8086880708d0d182a6e6d610c8a7cf9465e1f3c
bernadinedao <bernadine.dao@gmail.com>
2024-06-28 23:37:16 +1000

    update encode

--- src/utils/auth_api.js
+++ src/utils/auth_api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export async function findIdByEmail(email) {
+  const encodedEmail = encodeURIComponent(email);
   let res = await axios.get(
-    `${import.meta.env.VITE_BASE_URL}/api/user/${email}`
+    `${import.meta.env.VITE_BASE_URL}/api/user/${encodedEmail}`
   );
   return res.data;
 }
