2358ed8441a2060e3b39275bf288afc786d46ec2
bernadinedao <bernadine.dao@gmail.com>
2024-07-17 21:47:36 +1000

    remove commented out

--- vite.config.js
+++ vite.config.js
@@ -3,32 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "https://fridge-to-fork-api.onrender.com",
-  //       changeOrigin: true,
-  //       secure: false,
-  //     },
-  //     "/spoonacularApi": {
-  //       target: "https://fridge-to-fork-api.onrender.com",
-  //       changeOrigin: true,
-  //       secure: false,
-  //     },
-  //   },
-  // },
   base: "/fridge_to_fork/",
   build: {
     outDir: "docs",
   },
 });
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/api": "http://localhost:8080",
-//       "/spoonacularApi": "http://localhost:8080",
-//     },
-//   },
-// });
